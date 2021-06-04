400eb8c879cbe8d47defe8491640953d3269f69d
Yunyi Shen <34624472+YunyiShen@users.noreply.github.com>
2021-06-04 12:19:40 -0500

    Some vanilla result

--- R/Vanilla.R
+++ R/Vanilla.R
@@ -4,17 +4,19 @@ rstan_options(auto_write = TRUE)
 
 rawdata <- read.csv(list.files("./Data", full.names = TRUE)[1])
 
-timeseries <- rawdata$PDCSAP_FLUX[-1] # first entry is NA...
+#timeseries <- rawdata$PDCSAP_FLUX[-1] # first entry is NA...
+timeseries <- rawdata$PDCSAP_FLUX[6000:8000]
 observed <- 1 * (!is.na(timeseries))
 
 timeseries[is.na(timeseries)] <- timeseries[1]
+plot(timeseries)
 
 
 star_stan <- list(T=length(timeseries),
                     K=2, 
                     y = timeseries,
                     observed = observed, 
-                    alpha = c(1/2,1/2),
+                    alpha = c(1,1),
                     mu0=0,
                     lambda = 1e-10,
                     shape = 0.001,
@@ -34,7 +36,8 @@ fit <- mod$sample(
   data = star_stan,
   seed = 123,
   chains = 4,
-  parallel_chains = 4
+  parallel_chains = 4,
+  max_treedepth = 10
 )
 
 fit$save_object(file = "./Stan/vanilla_star.RDS")
