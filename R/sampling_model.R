70199f48402cbc7398c08c41f16341d98637d6c2
YunyiShen <yshen99@wisc.edu>
2021-06-12 15:09:08 -0500

    test pure QFDexN (w/o celerite)

--- R/sampling_model.R
+++ R/sampling_model.R
@@ -37,7 +37,7 @@ simuQFD <- function(N = 1000, theta_quiet = c(0.99,0.01),
             state <- sample(c(2,3),1, prob = theta_firing) # firing only goto firing and decay
         }
         else {
-            state <- sample(3,1,prob = theta_decay) # decay can go anywhere
+            state <- sample(c(1,2,3),1,prob = theta_decay) # decay can go anywhere
         }
         states[i] <- state
         if(state == 1){
@@ -48,6 +48,51 @@ simuQFD <- function(N = 1000, theta_quiet = c(0.99,0.01),
         }
         else {
             res[i] <- res[i-1] * rate_decay + sigma_decay * res[i]
+        }
+    }
+    return(list(timeseries = res, state = states))
+
+}
+
+# QFD with exponentially modified normal increase
+simuQFDexN <- function(N = 1000, theta_quiet = c(0.99,0.01), 
+                        theta_firing = c(0.6, 0.4), 
+                        theta_decay = c(0.1,0,0.95), 
+                        rate_firing = 0.1,
+                        rate_decay = 0.85, 
+                        sigma_noise = 2, 
+                        init = NULL,
+                        init_state = 1
+                        ){
+
+    if(is.null(init)){
+        init <- rnorm(1,0,sigma_noise)
+    }
+    state <- init_state
+    res <- rnorm(N,0,1)
+    states <- 0 * res
+    res[1] <- init
+    states[1] <- state
+    #browser()
+    for(i in 2:N){
+        if(state == 1){
+            state <- sample(c(1,2),1, prob = theta_quiet)# quiet only goto firing and quiet
+        }
+        else if (state==2) {
+            state <- sample(c(2,3),1, prob = theta_firing) # firing only goto firing and decay
+        }
+        else {
+            state <- sample(c(1,2,3),1,prob = theta_decay) # decay can go anywhere
+        }
+        states[i] <- state
+        if(state == 1){
+            res[i] <- sigma_noise * res[i]
+        }
+        else if (state==2) {
+            res[i] <- res[i-1] + rexp(1,rate_firing) + sigma_noise * res[i]
+        }
+        else {
+            res[i] <- res[i-1] * rate_decay + sigma_noise * res[i]
         }
     }
     return(list(timeseries = res, state = states))
