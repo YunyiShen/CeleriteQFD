df657b82e38405c705ad54d0b9fa3bbd3a9e5144
YunyiShen <yshen99@wisc.edu>
2021-09-13 18:44:04 -0500

    minor changes

--- R/misc.R
+++ R/misc.R
@@ -10,14 +10,15 @@ error_decoding <- function(decoding, states){
     decoding <- decoding > 1
 
     location_inj <- which(states)
-    res$n_injected <- sum((location_inj[-1]-location_inj[-length(location_inj)])!=1)
+    res$n_injected <- (length(location_inj)!=0)+sum((location_inj[-1]-location_inj[-length(location_inj)])!=1)
     location_decoding <- which(decoding)
-    res$n_detected <- sum((location_decoding[-1]-location_decoding[-length(location_decoding)])!=1)
+    res$n_detected <- (length(location_decoding)!=0)+sum((location_decoding[-1]-location_decoding[-length(location_decoding)])!=1)
 
     location_TP <- which( states & decoding)
-    res$TP <- sum((location_TP[-1]-location_TP[-length(location_TP)])!=1)
+    res$TP <- (length(location_TP)!=0)+sum((location_TP[-1]-location_TP[-length(location_TP)])!=1)
     location_FP <- which(!states & decoding)
-    res$FP <- sum((location_FP[-1]-location_FP[-length(location_FP)])!=1)
+    
+    res$FP <- (length(location_FP)!=0)+sum((location_FP[-1]-location_FP[-length(location_FP)])!=1)
     res$FN <- res$n_injected-res$TP
     res$SEN <- res$TP/res$n_injected
     res$SPC <- res$TP/res$n_detected
