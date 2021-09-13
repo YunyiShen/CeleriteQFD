f2442b84881af8ff8f849d4769300dfa1fcee9ce
YunyiShen <yshen99@wisc.edu>
2021-09-13 18:33:53 -0500

    add decoding evalation for simulations

--- R/misc.R
+++ R/misc.R
@@ -1,3 +1,27 @@
 majority <- function(x){
     as.numeric(names(which.max(table(x))))
 }
+
+error_decoding <- function(decoding, states){
+    res <- data.frame(matrix(NA,1,7))
+    colnames(res) <- c("n_detected","n_injected","TP","FP","FN","SEN","SPC")
+
+    states <- states > 1
+    decoding <- decoding > 1
+
+    location_inj <- which(states)
+    res$n_injected <- sum((location_inj[-1]-location_inj[-length(location_inj)])!=1)
+    location_decoding <- which(decoding)
+    res$n_detected <- sum((location_decoding[-1]-location_decoding[-length(location_decoding)])!=1)
+
+    location_TP <- which( states & decoding)
+    res$TP <- sum((location_TP[-1]-location_TP[-length(location_TP)])!=1)
+    location_FP <- which(!states & decoding)
+    res$FP <- sum((location_FP[-1]-location_FP[-length(location_FP)])!=1)
+    res$FN <- res$n_injected-res$TP
+    res$SEN <- res$TP/res$n_injected
+    res$SPC <- res$TP/res$n_detected
+    return(res)
+
+}
+
