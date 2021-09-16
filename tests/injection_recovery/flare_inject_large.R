b1bc0ebca0ed86020d4f70601be5949c82ef0021
Yunyi Shen <34624472+YunyiShen@users.noreply.github.com>
2021-09-15 19:05:23 -0500

    record all decoding in simulations

--- tests/injection_recovery/flare_inject_large.R
+++ tests/injection_recovery/flare_inject_large.R
@@ -1,5 +1,5 @@
 library(rstan)
-options(mc.cores = parallel::detectCores())
+options(mc.cores = parallel::detectCores()/2)
 rstan_options(auto_write = TRUE)
 source("./R/misc.R") # some helper
 source("./R/simuFlares.R")
@@ -10,11 +10,21 @@ rawdata[,2] <- rawdata[,2]-mean(rawdata[,2])
 
 res_path <- "./Res/Injection_recover/"
 file_num <- 1
-xm <- 50
+xm <- 10
 alpha <- 1
+offset <- 30
+upper <- 150
 n_inj <- 5
 t_half <- .00005
-res_file <- paste0(res_path,"inj_rec_",file_num,".csv")
+res_file <- paste0(res_path,"inj_rec_loss_",file_num,".csv")
+gtstate_file <- paste0(res_path,"inj_rec_gtstate_",file_num,".csv")
+flare_file <- paste0(res_path,"inj_rec_flare_",file_num,".csv")
+
+QFD_file <- paste0(res_path,"inj_rec_QFD_",file_num,".csv")
+sigma_file <- paste0(res_path,"inj_rec_3sigma_",file_num,".csv")
+
+
+
 
 n_rep <- 5
 i_res <- 1
@@ -37,14 +47,24 @@ modelcelerite <- stan_model(file = './Stan/Prototypes/Celerite/celerite.stan',
                              'celerite2/celerite2.hpp'), '"\n'))
 
 
-res_df <- data.frame(matrix(NA, n_res, 13))
-colnames(res_df) <- c("n_inj","t_half","xm","alpha","i_rep","method","n_detected","n_injected","TP","FP","FN","SEN","SPC")
+res_df <- data.frame(matrix(NA, n_res, 15))
+colnames(res_df) <- c("n_inj","t_half","xm","alpha","offset","upper","i_rep","method","n_detected","n_injected","TP","FP","FN","SEN","SPC")
+gtstate_df <- data.frame(matrix(NA, n_rep, N))
+flare_df <- data.frame(matrix(NA,n_rep,N))
+QFD_df <- data.frame(matrix(NA, n_rep, N-1))
+sigma_df <- data.frame(matrix(NA, n_rep, N))
 
-set.seed(12345)
+
+
+set.seed(42)
 for(i in 1:n_rep){
-    keplerflare_sim <- kepler_flare(rawdata[,1], t_half, n_inj,rPareto, xm, alpha)
+    keplerflare_sim <- kepler_flare(rawdata[,1], t_half, n_inj,rPareto, xm, alpha, offset, upper)
     injected <- rawdata
     injected[,2] <- injected[,2] + keplerflare_sim$flare
+    gtstate_df[i,] <- keplerflare_sim$states
+    flare_df[i,] <- keplerflare_sim$flare
+    write.csv(gtstate_df,gtstate_file)
+    write.csv(flare_df, flare_file)
 
     N <- nrow(injected)
 
@@ -65,30 +85,35 @@ for(i in 1:n_rep){
                 sigma_rate_firing = 1e3,
                 mu0_rate_decay = 0,
                 sigma_rate_decay = 1e3,
-                diag = rep(1e-6,N)
+                diag = rep(1e-6,N),
+                err_prior = c(.01,.01)
                 ) 
-    fitQFD <- sampling(modelQFD, data = QFD_data,control = list(adapt_delta = 0.9, max_treedepth=15), iter = 3000,init_r = 15, chains = 2)
+    fitQFD <- sampling(modelQFD, data = QFD_data,control = list(adapt_delta = 0.95, max_treedepth=15), iter = 2000,init_r = 15, chains = 2)
     QFD_samples <- as.data.frame(fitQFD)
     Viterbi_raw <- QFD_samples[,1:(N-1) + (N + 22)]
     Viterbi_max <- apply(Viterbi_raw,2,majority)
-    res_df[i_res,1:5] <- c(n_inj,t_half,xm,alpha,i)
+    res_df[i_res,1:7] <- c(n_inj,t_half,xm,alpha,offset,upper,i)
     res_df$method[i_res] <- "QFD"
-    res_df[i_res,7:13] <- error_decoding(Viterbi_max, keplerflare_sim$states)
+    res_df[i_res,9:15] <- error_decoding(Viterbi_max, keplerflare_sim$states[-1])
     i_res <- i_res + 1
     write.csv(res_df, res_file)
+    QFD_df[i,] <- Viterbi_max
+    write.csv(QFD_df,QFD_file)
+    gc(verbose = F,full = T)
+    
 
-    celeritedata <- QFD_data
-    celeritedata$err_prior <- c(0.01,0.01)
-
-    fitcelerite <- sampling(modelcelerite, data = celeritedata,control = list(adapt_delta = 0.9, max_treedepth=15), iter = 3000,init_r = 2, chains = 2)
+    fitcelerite <- sampling(modelcelerite, data = QFD_data,control = list(adapt_delta = 0.95, max_treedepth=15), iter = 2000,init_r = 2, chains = 2)
     summcelerite <- summary(fitcelerite)
     celerite_trend <- summcelerite[[1]][1:N + (N+23),1]
     residual <- rawdata[,2] - celerite_trend
 
     flares3sigma <- (residual >= (mean(residual) + 3 * sd(residual))) + 1
-    res_df[i_res,1:5] <- c(n_inj,t_half,xm,alpha,i)
+    res_df[i_res,1:7] <- c(n_inj,t_half,xm,alpha,offset,upper,i)
     res_df$method[i_res] <- "1-3-sigma"
-    res_df[i_res,7:13] <- error_decoding(flares3sigma, keplerflare_sim$states)
+    res_df[i_res,9:15] <- error_decoding(flares3sigma, keplerflare_sim$states)
     i_res <- i_res + 1
     write.csv(res_df, res_file)
+    gc(verbose = F,full = T)
+    sigma_df[i,] <- 1*flares3sigma
+    write.csv(sigma_df,sigma_file)
 }
